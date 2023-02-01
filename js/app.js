acfcfa4ef6dcac9e4709e04f8f136601af768746
Tanmay Kadam <justcoding1482@gmail.com>
2023-02-01 23:07:21 +0530

    Navbar Completed

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 /* --------------- Grab elements from DOM --------------- */
+const header = document.querySelector("header");
 
 /* --------------- Sticky Navbar --------------- */
+function stickyNavbar() {
+	header.classList.toggle("scrolled", window.pageYOffset > 0);
+	console.log(window.pageYOffset);
+}
+
+stickyNavbar();
+
+window.addEventListener("scroll", stickyNavbar);
 
 /* --------------- Reveal Animation --------------- */
 
